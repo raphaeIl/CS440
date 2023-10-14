3d5e50e512b3cc60b2a663634baa5171d18b1834
Michael <michaelsliu3@gmail.com>
2023-10-14 00:00:22 -0400

    project 1 submission

--- main.py
+++ main.py
@@ -10,24 +10,18 @@ def run_simulation(amount, ship_size, ship_flammability, bot_number, saved_ship_
         if result == TaskStatus.SUCCESS:
             success_simulations += 1
         else: # if fail, we wanna see what went wrong
-            sim.render()
+            sim.ship.render()
 
     return success_simulations / amount
 
 result = run_simulation(
-    amount=10, # number of simulations to run
+    amount=100, # number of simulations to run
     ship_size=20, # (D value)
-    ship_flammability=0.5, # (q value)
-    bot_number=4, # Bot1, Bot2, Bot3 or Bot4?
+    ship_flammability=1, # (q value)
+    bot_number=1, # Bot1, Bot2, Bot3 or Bot4?
     saved_ship_layout_file=None, # Use pre-generated ship layouts? (there is a 100 50x50 layouts in the saved_ship_layouts folder). Example: saved_ship_layout_file="layout_69"
     render_debug_logs=False, # displays a cute emoji representation of the simulation in the console ^v^
     # (best viewed in the visual studio code terminal, other terminals might mess up the formatting)
 )
 
 print("Performance: ", result * 100, "% sucess rate")
-
-# performance:
-# values between q 0 and 1
-# 
-# 
-
